d618986414a49d0e192bb3d63fa3f27e2c374a86
neweasth <168396145+neweasth@users.noreply.github.com>
2024-04-29 14:33:19 +0900

    Delete Node/1-22.js

--- Node/1-22.js
+++
@@ -1,8 +0,0 @@
-var n = null;
-console.log(typeof n); // object
-
-console.log(n == undefined); // true
-console.log(n == null); // true
-
-console.log(n === undefined); // false
-console.log(n === null); // true
